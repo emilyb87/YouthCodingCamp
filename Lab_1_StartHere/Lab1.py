e6a3e21d9ff14425a17c371114d809679dba6721
Emily Bernier <emilybernier87@gmail.com>
2022-11-11 18:47:03 -0700

    Lab 1 - ready

--- Lab_1_StartHere/Lab1.py
+++ Lab_1_StartHere/Lab1.py
@@ -23,15 +23,3 @@ if ((floor(int(number) / 2 ) * 2) == int(number)):
     print("Even number")
 else:
     print("Odd number")
-
-
-numberstring=input("Enter two numbers(x,x): ")
-#store two numbers in a List
-numberstring=numberstring.split(",")
-
-selection=input("Would you like to add these numbers or substract them?(A/S)")
-
-if (selection == "A"):
-    print(int(numberstring[0]) + int(numberstring[1]))
-elif (selection == "S"):
-    print(int(numberstring[0]) - int(numberstring[1]))
